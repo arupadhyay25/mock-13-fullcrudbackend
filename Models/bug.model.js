9bc00d4ef438849b8e748725aee0274a165fa54e
arupadhyay25 <arupadhyay25@gmail.com>
2023-01-19 12:32:20 +0530

    added

--- Models/bug.model.js
+++ Models/bug.model.js
@@ -3,7 +3,7 @@ let mongoose = require("mongoose");
 let bugSchema = mongoose.Schema({
   title: { type: String },
   status: { type: String },
-  userID: { type: String },
+  // userID: { type: String },
 });
 
 let BugModel = mongoose.model("bug_eval", bugSchema);
